f894a01de86fa5827140419b6689c43ad76ea508
Thomas <93293510+ta-torres@users.noreply.github.com>
2023-11-07 20:46:23 -0300

    Add getPlayerChoice()

--- script.js
+++ script.js
@@ -2,3 +2,25 @@ function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
     return choices[Math.floor(Math.random() * choices.length)];
 }
+
+function getPlayerChoice(){
+    let playerSelection = "";
+    playerSelection = prompt("Choose rock, paper, or scissors");
+
+    if (playerSelection == null){
+        console.log("Please enter rock, paper, or scissors");
+        return getPlayerChoice();
+    }
+    
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection === "rock" || 
+        playerSelection === "paper" || 
+        playerSelection === "scissors"){
+            return playerSelection;
+    }
+    else {
+        console.log("Please enter rock, paper, or scissors");
+        return getPlayerChoice();
+        
+    }
+}
