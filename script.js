592043cd63730eb3f87d5a31998bfbfa69c4c19d
Thomas <93293510+ta-torres@users.noreply.github.com>
2023-11-07 20:48:06 -0300

    Add playRound()

--- script.js
+++ script.js
@@ -24,3 +24,17 @@ function getPlayerChoice(){
         
     }
 }
+
+function playRound(playerSelection, computerSelection){
+    if (playerSelection === computerSelection){
+        return "It's a tie";
+    }
+    else if (playerSelection === "rock" && computerSelection === "scissors" ||
+        playerSelection === "paper" && computerSelection === "rock" ||
+        playerSelection === "scissors" && computerSelection === "paper"){
+            return `You win! ${playerSelection} beats ${computerSelection}`;
+    }
+    else {
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
+    }
+}
