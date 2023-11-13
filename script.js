534cb193d9a01fdac480c773c51c44c39b3c5e76
Thomas <93293510+ta-torres@users.noreply.github.com>
2023-11-13 18:56:19 -0300

    Rewrite player win conditions

--- script.js
+++ script.js
@@ -30,14 +30,18 @@ function getPlayerChoice(){
     return playerSelection;
 }
 
+function isPlayerWin(playerSelection, computerSelection){
+    return (playerSelection === "rock" && computerSelection === "scissors") ||
+           (playerSelection === "paper" && computerSelection === "rock") ||
+           (playerSelection === "scissors" && computerSelection === "paper");
+}
+
 function playRound(playerSelection, computerSelection){
     if (playerSelection === computerSelection){
         return "It's a tie";
     }
-    else if (playerSelection === "rock" && computerSelection === "scissors" ||
-        playerSelection === "paper" && computerSelection === "rock" ||
-        playerSelection === "scissors" && computerSelection === "paper"){
-            return `You win! ${playerSelection} beats ${computerSelection}`;
+    else if (isPlayerWin(playerSelection, computerSelection)){
+        return `You win! ${playerSelection} beats ${computerSelection}`;
     }
     else {
         return `You lose! ${computerSelection} beats ${playerSelection}`;
