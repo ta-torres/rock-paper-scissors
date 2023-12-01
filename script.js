e3b61e41267b5863a3d8a3d10aabfb064d7ac314
Thomas <93293510+ta-torres@users.noreply.github.com>
2023-12-01 14:09:40 -0300

    Remove old functions that are no longer needed

--- script.js
+++ script.js
@@ -7,33 +7,6 @@ function getComputerChoice() {
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
-function validatePlayerChoice(playerSelection) {
-    if (playerSelection === "rock" ||
-        playerSelection === "paper" ||
-        playerSelection === "scissors"){
-            return true;
-    }
-    else {
-        console.log("Please enter rock, paper, or scissors");
-        return false;
-    }
-}
-
-function getPlayerChoice(){
-    let playerSelection = "";
-    playerSelection = prompt("Choose rock, paper, or scissors");
-
-    if (playerSelection != null) {
-        playerSelection = playerSelection.toLowerCase();
-    }
-
-    if (!validatePlayerChoice(playerSelection)){
-        return getPlayerChoice();
-    }
-
-    return playerSelection;
-}
-
 function isPlayerWin(playerSelection, computerSelection){
     return (playerSelection === "rock" && computerSelection === "scissors") ||
            (playerSelection === "paper" && computerSelection === "rock") ||
@@ -61,38 +34,6 @@ function playRound(playerSelection, computerSelection){
         computerScore++;
     }
 }
-
-function game(){
-    let playerScore = 0;
-    let computerScore = 0;
-    let rounds = 1;
-
-    for (rounds; rounds <= 5; rounds++){
-        let playerSelection = getPlayerChoice();
-        let computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));
-
-        if (playRound(playerSelection, computerSelection).includes("win")){
-            playerScore++;
-        }
-        else if (playRound(playerSelection, computerSelection).includes("lose")){
-            computerScore++;
-        }
-    }
-    console.log(`Player score: ${playerScore}`);
-    console.log(`Computer score: ${computerScore}`);
-
-    if (playerScore > computerScore){
-        console.log("You win!");
-    }
-    else if (playerScore < computerScore){
-        console.log("You lose!");
-    }
-    else {
-        console.log("It's a tie!");
-    }
-}
-//game();
 
 const rockButton = document.getElementById("rock");
 const paperButton = document.getElementById("paper");
